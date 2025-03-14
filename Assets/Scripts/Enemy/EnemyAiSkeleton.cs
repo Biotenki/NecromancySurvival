6f6430fdd85a4a24d055aa5913337bf029d82097
Biotenki <Biotenky@gmail.com>
2025-03-14 11:47:03 +0000

    Nothing Really
    Added a menu scene, no real code, just basic visuals

---
+++ Assets/Scripts/Enemy/EnemyAiSkeleton.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAiSkeleton : MonoBehaviour
+{
+
+    [SerializeField]
+    float SightRange;
+    [SerializeField]
+    float LeashRange;
+    [SerializeField]
+    float Health;
+    [SerializeField]
+    float Damage;
+    [SerializeField]
+    float AttackRange;
+    [SerializeField]
+    float AttackDelay;
+   
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+    
+        
+
+
+
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/Player/PlayerSpells.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpells : MonoBehaviour
+{
+
+   
+
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+
+}
